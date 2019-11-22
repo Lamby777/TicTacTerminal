7c6be0af4d9e9232eb1d53c46727fb1034874593
Lamby777 <sheepareawesome777@gmail.com>
2019-11-22 15:56:44 +0000

    added gameplay loop

--- main.lua
+++ main.lua
@@ -1,7 +1,24 @@
+--io.read("*n")
+--------------------
+--    DexieT
+--------------------
+
+local gameplay = true
 local board = {
 	a, b, c,
 	d, e, f,
 	g, h, i
 }
-print(point["x"])
-print(point.x)
+
+while gameplay do
+	print([[Choose a box!
+	a|b|c
+	-----
+	d|e|f
+	-----
+	g|h|i]])
+	local box = io.read("*n");
+	for x in board do
+		--
+	end
+end
